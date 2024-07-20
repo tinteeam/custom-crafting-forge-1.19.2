78943e09971566b0fee410119ba3d1b97730a987
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-20 15:50:20 +0300

    added a custom item group

---
+++ src/main/java/org/tinteeam/customcrafting/item/ModCreativeModeTab.java
@@ -0,0 +1,16 @@
+package org.tinteeam.customcrafting.item;
+
+
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.MapItem;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab customcrafting = new CreativeModeTab("custom crafting items") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(item.BLANK_DIAMOND.get());
+        }
+    };
+}
--- src/main/java/org/tinteeam/customcrafting/item/item.java
+++ src/main/java/org/tinteeam/customcrafting/item/item.java
@@ -13,7 +13,7 @@ public class item {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CustomCrafting.MODID);
 
     public static final RegistryObject<Item> BLANK_DIAMOND = ITEMS.register("blank_diamond",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.customcrafting)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
