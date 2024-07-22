73ddeb1ee93704e5afdce01a7a44bd69cdf92ec4
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-22 13:02:21 +0300

    tried to use JEI

--- src/main/java/org/tinteeam/customcrafting/item/ModCreativeModeTab.java
+++ src/main/java/org/tinteeam/customcrafting/item/ModCreativeModeTab.java
@@ -7,7 +7,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MapItem;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab customcrafting = new CreativeModeTab("custom crafting items") {
+    public static final CreativeModeTab customcrafting = new CreativeModeTab("custom_crafting_items") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(item.BLANK_DIAMOND.get());
